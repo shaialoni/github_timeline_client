1c5c2412441547e5daf62c0cccb6e7f6b94d582e
amandacorral07 <amandalewis0707@gmail.com>
2022-09-02 13:15:53 -0700

    Added search bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
+import GitHubSearch from "./shared/GithubSearch"
+
 const Home = (props) => {
 	// const { msgAlert, user } = props
 	console.log('props in home', props)
 
 	return (
 		<>
-			<h2>Home Page</h2>
+			{/* <h2>Home Page</h2> */}
+			<GitHubSearch />
 		</>
 	)
 }
