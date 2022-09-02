1dd9c9a3074804ec0ce16db857aab09cab7e79a8
jamestylladams <jamestylladams@gmail.com>
2022-09-02 16:10:16 -0600

    adding upstream

--- src/components/shared/GithubSearch.js
+++ src/components/shared/GithubSearch.js
@@ -1,12 +1,32 @@
-// github search user component here
 import { Input, Button } from '@chakra-ui/react'
-
+import { useState } from 'react'
+import axios from 'axios'
 const GitHubSearch = () => {
+    console.log(process.env)
+    const [query, setQuery] = useState(null)
+
+    const gitHubSearch = () => {
+        console.log(process.env.REACT_APP_KEY)
+        axios.get(`https://api.github.com/users/${query}`, {
+            'headers': {
+                'Authorization': `token ${process.env.REACT_APP_KEY}` 
+              }
+        })
+        .then(res => {
+            console.log('RESPONSE', res.data)
+            console.log('')
+            console.log('name: ', res.data.name)
+        })
+    
+    }
+    const handleChange = (e) => {
+        setQuery(e.target.value)
+      };
 
 return(
     <>
-    <Input placeholder='Search Github' style={{marginTop: '30px', marginLeft: '20%', marginRight:'5px', width: '50%'}} />
-    <Button size='sm' colorScheme='linkedin'>Submit</Button>
+    <Input onChange={handleChange} placeholder='Search Github' style={{marginTop: '30px', marginLeft: '20%', marginRight:'5px', width: '50%'}} />
+    <Button onClick={gitHubSearch} size='sm' colorScheme='linkedin'>Submit</Button>
     </>
 )
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
+
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
